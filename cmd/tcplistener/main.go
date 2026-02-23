96fbd16ff663244b81a6df12242e3fe32d2aa539
LD-RW <basheermazari4@gmail.com>
2026-02-23 03:05:22 +0300

    used the request line (or start line) parser in the TCP reading and formatted the output

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/LD-RW/HTTPServer/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-
-			data = data[:n] // if the last read is less than 8 bytes
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str
-				str = ""
-			}
-			str += string(data)
-		}
-		if len(str) > 0 {
-			fmt.Printf("read: %s\n", str)
-		}
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
@@ -49,9 +18,15 @@ func main() {
 		if err != nil {
 			log.Fatal("error", "error", err)
 		}
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", "error", err)
 		}
+
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %v\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %v\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %v\n", r.RequestLine.HttpVersion)
 	}
 
 }
