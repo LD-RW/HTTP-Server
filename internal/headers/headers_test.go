590976cbefa315b637901d71c0161c33a2e60eef
LD-RW <basheermazari4@gmail.com>
2026-02-25 21:02:06 +0300

    added a case sensiitve case and handled multiple values for a single header testcase

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -23,9 +23,25 @@ func TestHeaders_Parse(t *testing.T) {
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	headers = NewHeaders()
 	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069,localhost:42069", headers.Get("HOST"))
+	assert.False(t, done)
+
 }
