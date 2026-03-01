cae27248b9cfbc01c2188322633d21e3d81fb18e
LD-RW <basheermazari4@gmail.com>
2026-03-01 23:05:47 +0300

    added more flexible function signatures for custom handlers and changed the server to reply with html instead of plain text

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -21,33 +20,20 @@ type HandlerError struct {
 	Message    string
 }
 
-type Handler func(w io.Writer, req *request.Request) *HandlerError
+type Handler func(w *response.Writer, req *request.Request)
 
 func runConnection(s *Server, conn io.ReadWriteCloser) {
 	defer conn.Close()
-
-	headers := response.GetDefaultHeaders(0)
+	responseWriter := response.NewWriter(conn)
 	r, err := request.RequestFromReader(conn)
 	if err != nil {
-		response.WriteStatusLine(conn, response.StatusBadRequest)
-		response.WriteHeaders(conn, headers)
+		responseWriter.WriteStatusLine(response.StatusBadRequest)
+		responseWriter.WriteHeaders(*response.GetDefaultHeaders(0))
 		return
 	}
-	writer := bytes.NewBuffer([]byte{})
-	handlerError := s.handler(writer, r)
-	var body []byte = nil
-	var status response.StatusCode = response.StatusOk
-	if handlerError != nil {
-		// TODO : I'm not pretty sure what to do here
-		status = handlerError.StatusCode
-		body = []byte(handlerError.Message)
-	} else {
-		body = writer.Bytes()
-	}
-	headers.Replace("Content-Length", fmt.Sprintf("%d", len(body)))
-	response.WriteStatusLine(conn, status)
-	response.WriteHeaders(conn, headers)
-	conn.Write(body)
+
+	s.handler(responseWriter, r)
+
 }
 
 func runServer(s *Server, listener net.Listener) error {
